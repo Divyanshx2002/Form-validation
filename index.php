bc314324192688bfe4481ceba7f60d8f14c2ac2a
Divyanshx2002 <divyansh235368@gmail.com>
2023-01-12 15:06:31 +0530

    second commit

--- index.php
+++ index.php
@@ -16,40 +16,42 @@
             <div class="heading">
                 <h1>Form</h1>
             </div>
+            <?php if (isset($_POST['submit'])) {
+                if ($_POST['fName'] == "") {
+                    echo "First Name is required";
+                }
+            } ?>
             <form id="mainform" class="form-1" action="submit.php" method="post">
                 <p class="ntpp"><label for="fName">First Name
-                        <input id="fName" name="fName" type="text" required>
+                        <input id="fName" name="fName" type="text" required autocomplete="off">
                     </label>
                 </p>
                 <p class="ntpp"><label for="lName">Last Name
-                        <input id="lName" name="lName" type="text" required>
+                        <input id="lName" name="lName" type="text">
                     </label>
                 </p>
                 <p class="ntpp"><label for="email">Email
-                        <input id="email" name="email" type="text" required>
+                        <input id="email" name="email" type="text">
                     </label>
                 </p>
 
                 <p class="ntpp"><label for="passWord">Password
-                        <input id="passWord"  class="password11" type="password" required>
+                        <input id="passWord" class="password11" name="password" type="password">
                     </label>
                 </p>
-
                 <p class="ntpp">
-                    <label   for="passWord2">Confirm Password
-                        <input id="passWo" class="password22" type="password" required>
+                    <label for="passWord2">Confirm Password
+                        <input id="passWo" class="password22" name="password" type="password">
                     </label>
                 </p>
-
                 <p class="opff">
                     <label for="male">Male
-                        <input type="radio" for= "gender" id="male" name="gender" value="Male">
+                        <input type="radio" for="gender" id="male" name="gender" value="Male">
                     </label>
                     <label for="html">Female
                         <input type="radio" id="female" name="gender" value="HTML">
                     </label>
                 </p>
-
                 <p>
                     <label for="movies">Choose A Mind Bending Movie</label>
                     <select id="movies" name="movies">
@@ -60,7 +62,7 @@
                     </select>
                 </p>
                 <p class="submitbtn">
-                    <input type="submit" value="Submit">
+                    <input type="submit" name="submit" value="Submit">
                 </p>
             </form>
         </div>
--- submit.php
+++ submit.php
@@ -1,7 +1,60 @@
 <?php
-echo "Your first name is: " . $_POST['fName'] . "<br>";
-echo "Your last name is: " . $_POST['lName'] . "<br>";
-echo "Your email is: " . $_POST['email'] . "<br>";
-echo "Your gender is: " . $_POST['gender'] . "<br>";
-echo "Your go to movie is:" . $_POST['movies'] . "<br>";
+if (isset($_POST['submit'])) {
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "learn";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $database);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO general (firstName, lastName, email, password, gender, movies) VALUES ('" . $_POST['fName'] . "', '" . $_POST['lName'] . "', '" . $_POST['email'] . "', '" . $_POST['password'] . "',  '" . $_POST['gender'] . "', '" . $_POST['movies'] . "')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
 ?>
+    <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        td,
+        th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+        tr:nth-child(even) {
+            background-color: #dddddd;
+        }
+    </style>
+    <table>
+        <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Gender</th>
+            <th>Movies</th>
+        </tr>
+        <tr>
+            <td class="sdfsdf"><?php echo $_POST['fName']; ?> </td>
+            <td><?php echo $_POST['lName']; ?></td>
+            <td><?php echo $_POST['email']; ?></td>
+            <td><?php echo $_POST['gender']; ?></td>
+            <td><?php echo $_POST['movies']; ?></td>
+        </tr>
+    </table>
+<?php } ?>
